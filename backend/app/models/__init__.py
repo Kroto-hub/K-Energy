d7ff473bc3ad99893260243147ed4c59d72da38e
Kroto-hub <1727063293@qq.com>
2026-04-24 18:02:24 +0800

    feat: add construction calculation log model

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,3 +1,33 @@
-from app.models.project import Project, EnergyStation, Equipment, LoadProfile, CalculationResult
+from app.models.project import Project, ProjectBuilding, EnergyStation, Equipment, LoadProfile, CalculationResult
+from app.models.scheme import ProjectScheme, SchemeEquipment, SchemeStorageUnit, SchemeTariffBinding
+from app.models.simulation import SimulationJob
+from app.models.catalog import WeatherCity, WeatherEpwFile, EquipmentCatalog, ThermalStorageCatalog
+from app.models.template_library import (
+    ConstructionCategory,
+    ConstructionCalculationLog,
+    ConstructionLayer,
+    ConstructionTemplate,
+    HolidayCalendar,
+    HolidayEntry,
+    MaterialCategory,
+    MaterialLibrary,
+    ScheduleTemplate,
+    ScheduleTemplateGroup,
+    SeasonRule,
+    SeasonRuleSet,
+    UsageTemplate,
+    UsageTemplateCategory,
+)
 
-__all__ = ["Project", "EnergyStation", "Equipment", "LoadProfile", "CalculationResult"]
+__all__ = [
+    "Project", "ProjectBuilding", "EnergyStation", "Equipment", "LoadProfile", "CalculationResult",
+    "ProjectScheme", "SchemeEquipment", "SchemeStorageUnit", "SchemeTariffBinding",
+    "SimulationJob",
+    "WeatherCity", "WeatherEpwFile", "EquipmentCatalog", "ThermalStorageCatalog",
+    "UsageTemplateCategory", "UsageTemplate",
+    "ScheduleTemplateGroup", "ScheduleTemplate",
+    "HolidayCalendar", "HolidayEntry",
+    "SeasonRuleSet", "SeasonRule",
+    "ConstructionCategory", "ConstructionTemplate", "ConstructionCalculationLog",
+    "MaterialCategory", "MaterialLibrary", "ConstructionLayer",
+]
